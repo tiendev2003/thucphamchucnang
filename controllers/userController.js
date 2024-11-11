3ef1f9b6b913342d01dc6de14096d6ffe8a633cf
tiendev2003 <tientc.21it@vku.udn.vn>
2024-11-11 12:03:09 +0700

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -89,3 +89,40 @@ exports.loginManager = async (req, res, next) => {
     next(error);
   }
 }
+
+exports.getUserInfo = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ where: { id: req.user.id } });
+    if (!user) {
+      return res.status(404).json(formatResponse('User not found', null));
+    }
+    res.status(200).json(formatResponse('User retrieved successfully', user));
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.updateUserInfo = async (req, res, next) => {
+  try {
+    const { name, email, phone, password } = req.body;
+    const user = await User.findOne({ where: { id: req.user.id } });
+    if (!user) {
+      return res.status(404).json(formatResponse('User not found', null));
+    }
+
+    // Update user fields
+    user.name = name || user.name;
+    user.email = email || user.email;
+    user.phone = phone || user.phone;
+
+    // Hash new password if provided
+    if (password) {
+      user.password = await bcrypt.hash(password, 10);
+    }
+
+    await user.save();
+    res.status(200).json(formatResponse('User updated successfully', user));
+  } catch (error) {
+    next(error);
+  }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const { register, login,loginManager } = require("../controllers/userController");
+const { register, login,loginManager, getUserInfo, updateUserInfo } = require("../controllers/userController");
+const { authenticate } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/login-manager", loginManager);
+router.get("/info", authenticate, getUserInfo);
+router.put("/update", authenticate, updateUserInfo);
 module.exports = router;
